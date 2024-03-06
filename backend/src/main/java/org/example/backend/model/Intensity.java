a85c1d37120d75f7ee0b4efaf098efa0bec62dd7
Lüder Budde <lueder.budde@gmail.com>
2024-03-06 12:42:16 +0100

    Add Intensity enum, Workout class, WorkoutRepo class and UserData class

---
+++ backend/src/main/java/org/example/backend/model/Intensity.java
@@ -0,0 +1,5 @@
+package org.example.backend.model;
+
+public enum Intensity {
+    LOW, MEDIUM, HIGH
+}
---
+++ backend/src/main/java/org/example/backend/model/UserData.java
@@ -0,0 +1,9 @@
+package org.example.backend.model;
+
+public class UserData {
+    private int weight;
+    private int height;
+    private int age;
+    private int desiredWeight;
+    private int desiredPeriodForWeightLoss;
+}
