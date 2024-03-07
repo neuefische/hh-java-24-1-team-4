692481d801b5ec354bccb1853e09219c19093ff3
R-Schreyer <roland_schreyer@web.de>
2024-03-07 12:02:22 +0100

    add class User

--- backend/src/main/java/org/example/backend/repository/UserRepository.java
+++ backend/src/main/java/org/example/backend/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package org.example.backend.repository;
 
-import org.example.backend.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
