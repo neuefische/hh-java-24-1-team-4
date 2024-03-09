4d55d534e44993cec13046ae3a68232bf98cbe16
siqichen <siqi1987@hotmail.com>
2024-03-09 19:19:58 +0100

    add one test for getAllUser Controller

---
+++ backend/src/test/java/org/example/backend/controller/UserControllerTest.java
@@ -0,0 +1,37 @@
+package org.example.backend.controller;
+
+import org.example.backend.model.User;
+import org.example.backend.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private UserRepository userRepo;
+
+    @Test
+    void getAllUsers_shouldReturnEmptyList_WhenCalledInitially() throws Exception {
+        //GIVEN
+        List<User> userList=List.of();
+        //WHEN&THEN
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("[]"));
+    }
+
+
+}
