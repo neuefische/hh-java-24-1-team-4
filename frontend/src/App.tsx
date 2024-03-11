89da657ae428150fee489eb986c0eb3f1117c7e0
Lüder Budde <lueder.budde@gmail.com>
2024-03-11 16:43:12 +0100

    add Workout List and create Backend Controller, Service, Repo and Type for Workouts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,32 @@
 import './App.css'
 import Layout from "./components/Layout/Layout.tsx";
 import UserProfile from "./components/UserProfile/UserProfile.tsx";
-import WorkoutList from "./components/WorkoutList/WorkoutList.tsx";
+import Workouts from "./components/Workouts/Workouts.tsx";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {Workout} from "./Types/Workout.ts";
 import {Route, Routes} from "react-router-dom";
+import WorkoutDetailsPage from "./components/WorkoutDetailsPage/WorkoutDetailsPage.tsx";
 import Workoutplan from "./components/Workoutplan/Workoutplan.tsx";
 
 
 
 export default function App() {
 
-
-
-const [workouts, setWorkouts] = useState<Workout[]>([])
+    const [workouts, setWorkouts] = useState<Workout[]>([])
 
     function fetchWorkouts() {
-        axios.get("api/workout")
+        axios.get("/api/workout")
             .then(response => {
                 console.log("Response: ", response.data);
                 setWorkouts(response.data);
             })
             .catch(error => console.log("Error fetching data: ", error))
     }
+    console.log("Workouts: ", workouts);
+
     useEffect(() => {
         fetchWorkouts();
-        console.log("Workouts: ", workouts);
     } ,[])
 
   return (
@@ -35,6 +35,9 @@ const [workouts, setWorkouts] = useState<Workout[]>([])
              <Route path="/userprofile" element={<UserProfile/>}/>
              <Route path="/workouts" element={<WorkoutList/>}/>
              <Route path="/workoutplan" element={<Workoutplan/>}/>
+             <Route path="/user" element={<UserProfile/>}/>
+             <Route path="/workouts" element={<Workouts workouts={workouts}/>}/>
+             <Route path="/workouts/:id" element={<WorkoutDetailsPage/> }/>
          </Routes>
         {/*<UserProfile/>*/}
          {/*<Signin/>*/}
--- frontend/src/components/WorkoutDetailsPage/WorkoutDetailsPage.tsx
+++ frontend/src/components/WorkoutDetailsPage/WorkoutDetailsPage.tsx
@@ -1,17 +1,53 @@
-import 'WorkoutDetailsPage.css'
+import './WorkoutDetailsPage.css';
 import {Workout} from "../../Types/Workout.ts";
+import {Navigate, useNavigate, useParams} from "react-router-dom";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
-type WorkoutDetailsPageProps = {
-    workout: Workout
-}
+export default function WorkoutDetailsPage(){
 
-export default function WorkoutDetailsPage({workout}: Readonly<WorkoutDetailsPageProps>){
+    const [workout, setWorkout] = useState<Workout | null | undefined >(undefined);
+    const params = useParams();
+    const id: string | undefined = params.id;
+
+    const navigate = useNavigate();
+
+    function navigateToAllWorkouts() {
+        navigate("/workouts");
+    }
+
+    function fetchWorkoutById() {
+        axios.get(`/api/workout/${id}`)
+            .then(response => {
+                console.log("Response: ", response.data);
+                setWorkout(response.data);
+            })
+            .catch(error => {
+                console.log("Error fetching data: ", error);
+                setWorkout(null);
+                }
+                )
+    }
+
+    useEffect(() => {
+        fetchWorkoutById();
+    }, [])
+
+    if (workout === null) {
+        return <Navigate to="/workouts"/>
+    }
+    if (workout === undefined) {
+        return <p>loading ... </p>
+    }
+    console.log("Workout: ", workout);
   return (
-    <div className="WorkoutDetailsPage">
-      <h1>{workout.name} Details</h1>
+    <div className="workoutDetailsPage">
+        <h1>{workout.name} Details</h1>
         <p>{workout.description}</p>
         <p>Intensity: {workout.intensity}</p>
         <p>Duration in min: {workout.duration}</p>
+        <button onClick={navigateToAllWorkouts}>back to all Workouts</button>
     </div>
+
   )
 }
