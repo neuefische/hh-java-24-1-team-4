ef03894d1a31633c8d874511f4a2bd1d1235444c
R-Schreyer <roland_schreyer@web.de>
2024-03-12 17:26:45 +0100

    add feature that displays profile data on profile screen

--- frontend/src/components/UserProfile/UserProfile.tsx
+++ frontend/src/components/UserProfile/UserProfile.tsx
@@ -1,23 +1,21 @@
 import './UserProfile.css'
 import {UserData} from "../../Types/UserData.ts";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 import axios from 'axios';
-import {FaRegUser} from "react-icons/fa";
-import {MdOutlineEmail} from "react-icons/md";
-import {RiLockPasswordLine} from "react-icons/ri";
+
 
 export default function UserProfile() {
 
-    const [userData, setUserData] = useState<UserData>({
+    const [profileData, setProfileData] = useState<UserData>()
+    /*const [userData, setUserData] = useState<UserData>({
         firstName: "", lastName: "", email: "", password: ""
-    });
-    const [allUsers, setAllUsers] = useState<UserData[]>([]);
-    function updateInputFields(event: React.ChangeEvent<HTMLInputElement>) {
+    });*/
+    /*function updateInputFields(event: React.ChangeEvent<HTMLInputElement>) {
         event.preventDefault();
         setUserData({...userData, [event.target.name]: event.target.value});
-    }
-    const postUserData = (userData: UserData) => {
+    }*/
+    /*const postUserData = (userData: UserData) => {
         axios.post("api/user",
             userData
         )
@@ -25,35 +23,16 @@ export default function UserProfile() {
                 console.log("Response: ", response);
             })
             .catch(error => console.log("Error posting data: ", error))
-    }
-
-    const fetchUserData = () => {
-        axios.get("api/user")
-            .then(response => {
-                console.log("Response: ", response.data);
-                setAllUsers(response.data);
-            })
-            .catch(error => console.log("Error fetching data: ", error))
-    }
+    }*/
 
     const fetchUserById = (id: string) => {
-        axios.get(`api/user/${id}`)
-            .then(response => {
-                console.log("Response: ", response.data.firstName);
-            })
-            .catch(error => console.log("Error fetching data: ", error))
+            axios.get(`api/user/${id}`)
+                .then(response => {
+                    setProfileData(response.data)
+                })
+                .catch(error => console.log("Error fetching data: ", error))
     }
 
-    function updateUserData(userData: UserData) {
-        axios.put(`api/user/${userData._id}`,
-            userData
-        )
-            .then(response => {
-                console.log("Response: ", response.data.firstName);
-                setUserData(response.data)
-            })
-            .catch(error => console.log("Error fetching data: ", error))
-    }
 
     /*const deleteUserById = (id: string) => {
         axios.delete(`api/user/${id}`)
@@ -64,32 +43,25 @@ export default function UserProfile() {
             .catch(error => console.log("Error fetching data: ", error))
     }*/
 
+    useEffect(() => {
+        fetchUserById("1")
+    }, []);
+
   return (
       <div className="UserCard">
           <h1>My Profile</h1>
           <div className="UserProfile">
-          <p>Firstname: {userData.firstName}</p>
-          <p>Lastname: {userData.lastName}</p>
-          <p>Email: {userData.email}</p>
-          <p>Password: {userData.password}</p>
-          </div>
-          <div>
-              <ul>
-                    {allUsers.map((user) => (
-                        <li key={user._id}>
-                            {user.firstName}
-                            {/*<button onClick={deleteUserById(user._id)}>Delete</button>*/}
-                        </li>
-
-                    ))}
-                </ul>
+            <p>Firstname: {profileData?.firstName}</p>
+            <p>Lastname: {profileData?.lastName}</p>
+            <p>Email: {profileData?.email}</p>
+            <p>Password: {profileData?.password}</p>
           </div>
 
-            <button onClick={() => fetchUserData()}>Fetch</button>
+            {/*<button onClick={() => fetchUserData()}>Fetch</button>
             <button onClick={() => fetchUserById("1")}>Fetch 1</button>
             <button onClick={() => fetchUserById("2")}>Fetch 2</button>
-            <button onClick={() => updateUserData(userData)}>Update</button>
-          <form className="userDataInputForm">
+            <button onClick={() => updateUserData(userData)}>Update</button>*/}
+          {/*<form className="userDataInputForm">
               <div className="input-field">
                   <div className="icon"><FaRegUser/></div>
                   <input type="text" name="firstName" value={userData.firstName} placeholder="First Name" onChange={updateInputFields}/>
@@ -107,7 +79,7 @@ export default function UserProfile() {
                   <input type="password" name="password" value={userData.password} placeholder="Password" onChange={updateInputFields}/>
               </div>
               <button className="save" onClick={() => postUserData(userData)}>Save</button>
-          </form>
+          </form>*/}
       </div>
   )
 }
