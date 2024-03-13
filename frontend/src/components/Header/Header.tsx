238b65591d10f54de0ca958172c102a990f10b29
Luederb <lueder.budde@gmail.com>
2024-03-13 14:53:06 +0100

    Login with GitHub. Add Backend Routes and get GitHubUserId

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,17 +2,11 @@ import './Header.css'
 
 export default function Header() {
 
-    function login() {
-        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080': window.location.origin
 
-        window.open(host + '/oauth2/authorization/github', '_self')
-    }
 
     return (
         <header className="header">
-            <div></div>
             <h1 style={{color: "white"}}>Fitness-App</h1>
-            <button onClick={login}>login</button>
         </header>
     )
 }
