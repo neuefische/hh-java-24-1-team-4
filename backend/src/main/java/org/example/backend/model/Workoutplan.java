fd89283e1464b208cf4200e0532b754781837953
Luederb <lueder.budde@gmail.com>
2024-03-14 10:55:55 +0100

    Create basic files for Backend to handle the Workoutplan

---
+++ backend/src/main/java/org/example/backend/model/Workoutplan.java
@@ -0,0 +1,17 @@
+package org.example.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+@Builder
+public class Workoutplan {
+    private Workout monday;
+    private Workout tuesday;
+    private Workout wednesday;
+    private Workout thursday;
+    private Workout friday;
+    private Workout saturday;
+    private Workout sunday;
+}
---
+++ backend/src/main/java/org/example/backend/service/WorkoutplanService.java
@@ -0,0 +1,4 @@
+package org.example.backend.service;
+
+public class WorkoutplanService {
+}
