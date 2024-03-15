f8adf78087c0dcde13f15bfc5e760b7d49654003
R-Schreyer <roland_schreyer@web.de>
2024-03-15 16:02:30 +0100

    refactore calculations

--- frontend/src/Types/WorkoutPlan.ts
+++ frontend/src/Types/WorkoutPlan.ts
@@ -30,3 +30,33 @@ export type WorkoutPlan = {
         duration: number;
     };
 }
+export type  NewWorkoutplan = {
+    monday?: {
+        workout?: Workout;
+        duration?: number;
+    };
+    tuesday?: {
+        workout?: Workout;
+        duration?: number;
+    };
+    wednesday?: {
+        workout?: Workout;
+        duration?: number;
+    };
+    thursday?: {
+        workout?: Workout;
+        duration?: number;
+    };
+    friday?: {
+        workout?: Workout;
+        duration?: number;
+    };
+    saturday?: {
+        workout?: Workout;
+        duration?: number;
+    };
+    sunday?: {
+        workout?: Workout;
+        duration?: number;
+    };
+}
